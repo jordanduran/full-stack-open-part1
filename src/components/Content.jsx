b7b99655a3ff13e49dc24881e868587215e8e7f5
Jordan Duran <jordanduranm@gmail.com>
2024-01-22 13:34:05 -0500

    Exercise 1.3

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,30 +1,20 @@
 import PropTypes from 'prop-types';
 import Part from './Part';
 
-const Content = ({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3,
-}) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </>
   );
 };
 
 Content.propTypes = {
-  part1: PropTypes.string.isRequired,
-  exercises1: PropTypes.number.isRequired,
-  part2: PropTypes.string.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  part3: PropTypes.string.isRequired,
-  exercises3: PropTypes.number.isRequired,
+  part1: PropTypes.object.isRequired,
+  part2: PropTypes.object.isRequired,
+  part3: PropTypes.object.isRequired,
 };
 
 export default Content;
