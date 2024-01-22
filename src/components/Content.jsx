8d7a76d7c1cdc04186951235cab05957c1df8b7b
Jordan Duran <jordanduranm@gmail.com>
2024-01-22 14:02:13 -0500

    Exercise 1.4

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 import Part from './Part';
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
+      {parts.map((part) => {
+        return (
+          <Part key={part.name} part={part.name} exercises={part.exercises} />
+        );
+      })}
     </>
   );
 };
 
 Content.propTypes = {
-  part1: PropTypes.object.isRequired,
-  part2: PropTypes.object.isRequired,
-  part3: PropTypes.object.isRequired,
+  parts: PropTypes.array.isRequired,
 };
 
 export default Content;
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = ({ parts }) => {
+  const getCount = () => {
+    let count = 0;
+    parts.forEach((part) => {
+      count += part.exercises;
+    });
+
+    return count;
+  };
+
+  return <p>Number of exercises {getCount()}</p>;
 };
 
 Total.propTypes = {
-  exercises1: PropTypes.number.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  exercises3: PropTypes.number.isRequired,
+  parts: PropTypes.array.isRequired,
 };
 
 export default Total;
